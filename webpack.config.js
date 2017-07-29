a754c0737452972e711a95afb826ad32219d9b53
PakkuDon <pakku.pd@gmail.com>
2017-07-29 23:59:55 +1000

    Add Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path')
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/assets/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
