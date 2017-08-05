fe99a36a1de156c680843afa6bbf3c9cd1ec4028
PakkuDon <pakku.pd@gmail.com>
2017-08-05 23:57:39 +1000

    Implement basic timer

--- src/app.js
+++ src/app.js
@@ -1,2 +1,25 @@
 import 'normalize'
 import './styles.css'
+
+const timerDisplay = document.querySelector('.timer')
+const timerProgress = document.querySelector('progress')
+
+let initialDuration = 25 * 60 * 1000
+let timeRemaining = initialDuration
+
+let animationID = setInterval(() => {
+  timeRemaining -= 1000
+
+  if (timeRemaining < 0) {
+    timeRemaining = 0
+    clearInterval(animationID)
+  }
+
+  const minutes = Math.floor(timeRemaining / (60 * 1000))
+  const seconds = (timeRemaining / 1000) % 60
+  const remainingTimeFormatted = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
+
+  timerDisplay.textContent = remainingTimeFormatted
+  timerProgress.max = initialDuration
+  timerProgress.value = initialDuration - timeRemaining
+}, 1000)
