53582e683d72bbebbc91657608813eccf02e3e5a
PakkuDon <pakku.pd@gmail.com>
2017-08-16 00:16:15 +1000

    Move UI concerns into separate functions

--- src/app.js
+++ src/app.js
@@ -15,21 +15,26 @@ let animationID
 startButton.addEventListener('click', () => {
   clearInterval(animationID)
 
-  let workDuration = parseInt(workDurationInput.value)
-  let breakDuration = parseInt(breakDurationInput.value)
+  let workDuration = getWorkDuration()
+  let breakDuration = getBreakDuration()
   Timer.start(workDuration, breakDuration)
+  displayTimeRemaining(Timer.getTimeRemaing(), Timer.getDuration())
 
   animationID = setInterval(() => {
     Timer.tick()
-    const timeRemaining = Timer.getTimeRemaing();
-    const timeDuration = Timer.getDuration()
-
-    const minutes = Math.floor(timeRemaining / (60 * 1000))
-    const seconds = Math.round((timeRemaining / 1000) % 60)
-    const remainingTimeFormatted = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
-
-    timerDisplay.textContent = remainingTimeFormatted
-    timerProgress.max = timeDuration
-    timerProgress.value = timeDuration - timeRemaining
+    displayTimeRemaining(Timer.getTimeRemaing(), Timer.getDuration())
   }, 1000)
 })
+
+const getWorkDuration = () => parseInt(workDurationInput.value)
+const getBreakDuration = () => parseInt(breakDurationInput.value)
+
+const displayTimeRemaining = (timeRemaining, duration) => {
+  const minutes = Math.floor(timeRemaining / (60 * 1000))
+  const seconds = Math.round((timeRemaining / 1000) % 60)
+  const remainingTimeFormatted = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
+
+  timerDisplay.textContent = remainingTimeFormatted
+  timerProgress.max = duration
+  timerProgress.value = duration - timeRemaining
+}
