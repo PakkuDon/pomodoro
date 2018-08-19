dde9a3d5b75b1ece0d5bacc8aec8094f594f7945
PakkuDon <pakku.pd@gmail.com>
2018-08-19 20:23:45 +1000

    Add React-Redux Provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import Pomodoro from './containers/Pomodoro'
+import reducer from './reducers'
 
 import './styles.css'
 
-ReactDOM.render(<Pomodoro />, document.querySelector('#root'))
+const store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Pomodoro />
+  </Provider>,
+  document.querySelector('#root'),
+)
