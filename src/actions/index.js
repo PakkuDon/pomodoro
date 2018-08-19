9be666c00238afd5d04da58b60db4abfb5681a86
PakkuDon <pakku.pd@gmail.com>
2018-08-19 21:18:47 +1000

    Add action creators for settings and timer actions

---
+++ src/actions/index.js
@@ -0,0 +1,25 @@
+import * as ActionTypes from './types'
+
+export const startTimer = (workLength, breakLength) => ({
+  type: ActionTypes.START_TIMER,
+  workLength,
+  breakLength,
+})
+
+export const stopTimer = () => ({
+  type: ActionTypes.STOP_TIMER,
+})
+
+export const tick = () => ({
+  type: ActionTypes.TIMER_TICK,
+})
+
+export const flipPanel = () => ({
+  type: ActionTypes.FLIP_PANEL,
+})
+
+export const editSetting = (field, value) => ({
+  type: ActionTypes.EDIT_SETTING,
+  field,
+  value,
+})
---
+++ src/actions/types.js
@@ -0,0 +1,6 @@
+export const START_TIMER = 'START_TIMER'
+export const STOP_TIMER = 'STOP_TIMER'
+export const TIMER_TICK = 'TIMER_TICK'
+
+export const FLIP_PANEL = 'FLIP_PANEL'
+export const EDIT_SETTING = 'EDIT_SETTING'
