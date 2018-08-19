8840693be6faf3d653a53660530d062124f75929
PakkuDon <pakku.pd@gmail.com>
2018-08-19 20:58:03 +1000

    Add Redux DevTools extension

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import Pomodoro from './containers/Pomodoro'
 import reducer from './reducers'
 
 import './styles.css'
 
-const store = createStore(reducer)
+const store = createStore(reducer, composeWithDevTools())
 
 ReactDOM.render(
   <Provider store={store}>
