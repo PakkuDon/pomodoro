44d5c28f51da5b24f4c823c9516398cc5469e8ff
PakkuDon <pakku.pd@gmail.com>
2018-09-23 01:12:21 +1000

    Split reducer/timer handlers into separate functions

--- src/reducers/timer.js
+++ src/reducers/timer.js
@@ -1,50 +1,57 @@
 import * as ActionTypes from '../actions/types'
 import initialState from './initialState'
 
+const startTimer = (state, action) => {
+  const { workLength, breakLength } = action
+  const intervals = [
+    {
+      name: 'Work',
+      length: parseInt(workLength, 10) * 60 * 1000,
+      count: 0,
+    },
+    {
+      name: 'Break',
+      length: parseInt(breakLength, 10) * 60 * 1000,
+      count: 0,
+    },
+  ]
+
+  return {
+    ...state,
+    intervals,
+    lastTick: Date.now(),
+    timeRemaining: intervals[0].length,
+    currentIntervalIndex: 0,
+  }
+}
+
+const timerTick = (state) => {
+  const newTick = Date.now()
+  const timeRemaining = state.timeRemaining - (newTick - state.lastTick)
+
+  if (timeRemaining > 0) {
+    return {
+      ...state,
+      timeRemaining,
+      lastTick: newTick,
+    }
+  }
+  
+  const newIntervalIndex = (state.currentIntervalIndex + 1) % state.intervals.length
+  return {
+    ...state,
+    currentIntervalIndex: newIntervalIndex,
+    timeRemaining: state.intervals[newIntervalIndex].length,
+    lastTick: newTick,
+  }
+}
+
 export default (state = initialState.timer, action) => {
   switch (action.type) {
     case ActionTypes.START_TIMER:
-      const { workLength, breakLength } = action
-      const intervals = [
-        {
-          name: 'Work',
-          length: parseInt(workLength, 10) * 60 * 1000,
-          count: 0,
-        },
-        {
-          name: 'Break',
-          length: parseInt(breakLength, 10) * 60 * 1000,
-          count: 0,
-        },
-      ]
-
-      return {
-        ...state,
-        intervals,
-        lastTick: Date.now(),
-        timeRemaining: intervals[0].length,
-        currentIntervalIndex: 0,
-      }
+      return startTimer(state, action)
     case ActionTypes.TIMER_TICK:
-      const newTick = Date.now()
-      const timeRemaining = state.timeRemaining - (newTick - state.lastTick)
-
-      if (timeRemaining > 0) {
-        return {
-          ...state,
-          timeRemaining,
-          lastTick: newTick,
-        }
-      }
-      else {
-        const newIntervalIndex = (state.currentIntervalIndex + 1) % state.intervals.length
-        return {
-          ...state,
-          currentIntervalIndex: newIntervalIndex,
-          timeRemaining: state.intervals[newIntervalIndex].length,
-          lastTick: newTick,
-        }
-      }
+      return timerTick(state)
     default:
       return state
   }
